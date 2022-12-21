b6d83af2b08f97d011be2dbb9e208c0aa698733e
Brooke hill <brookeannstasia@gmail.com>
2022-12-21 10:58:56 -0500

    Makeesha

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
-const TASKS = [
+const [Tasks,setTasks] =  useState([
   {
     id: 1,
     title: 'Mow the lawn',
@@ -13,16 +13,19 @@ const TASKS = [
     title: 'Cook Pasta',
     isComplete: true,
   },
-];
+]);
 
 const App = () => {
+  const updateTask = () => {
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={Tasks} />}</div>
       </main>
     </div>
   );
